499841f07be439bf8e82c7f960c95415b13d94e1
zohan syah fatomi <expeliarnus@gmail.com>
2018-12-30 08:30:24 +0700

    Add files via upload

---
+++ 4_supercellmon57_construction/code.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#PBS -N  SN_Phonon
+#PBS -o  phase0.out
+#PBS -e  phase0.err
+#PBS -q  nongpu
+#PBS -l  walltime=24:00:00
+#PBS -l  nodes=8:ppn=8
+
+module load openmpi
+module load gcc/4.8.5
+module load blas
+module load boost
+module load fftw
+module load phase0/2014.03
+
+cd $PBS_O_WORKDIR
+mpirun -np 64 phase ne=64 nk=1
