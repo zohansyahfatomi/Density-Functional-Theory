402965f397fa3d252bfc439d9826be394978135a
zohan syah fatomi <expeliarnus@gmail.com>
2019-01-04 17:35:48 +0700

    Add files via upload

---
+++ 9_unitcell_k-poinkonvergen/k1/phase0.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH --job-name="opt_unitcell"
+#SBATCH --mail-type=END
+#   TIPE: BEGIN, END, FAIL, REQUEUE
+#SBATCH --mail-user=zohan.syah.f@mail.UGM.AC.ID
+#SBATCH --nodes=4
+#SBATCH --ntasks=4
+#SBATCH --ntasks-per-core=1
+#SBATCH --output=testingphase0.out
+module load mpi/openmpi-x86_64
+module load apps/phase0
+#!cd testrun
+mpirun phase 
---
+++ 9_unitcell_k-poinkonvergen/k2/phase0.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH --job-name="k2"
+#SBATCH --mail-type=END
+#   TIPE: BEGIN, END, FAIL, REQUEUE
+#SBATCH --mail-user=zohan.syah.f@mail.UGM.AC.ID
+#SBATCH --nodes=4
+#SBATCH --ntasks=4
+#SBATCH --ntasks-per-core=1
+#SBATCH --output=testingphase0.out
+module load mpi/openmpi-x86_64
+module load apps/phase0
+#!cd testrun
+mpirun phase 
